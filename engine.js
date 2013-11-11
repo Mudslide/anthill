c642bc2c9d3f3bfe4fdf8735023e8520144e9066
m93a <m93a.cz@gmail.com>
2013-11-11 12:08:13 +0100

    js -> py

--- engine.js
+++ engine.js
@@ -1,51 +1,40 @@
-//http://www.redblobgames.com/grids/hexagons/
-win.World = {
- "map": [],
- "xy": function(x,y){
-  if(map[x,y]){
-   return map[x,y];
-  }else{
-   return false;
-  }
- },
- "qr": function(q,r){
-  y = q+Math.ceil(r/2);
-  if(map[r,y]){
-   return map[r,y];
-  }else{
-   return false;
-  }
- }
-};
-win.Node = function(x,y){
- if(!this instanceof Node){return new Node(x,y);}
- this.domNode = doc.mkNode("","cell");
- this.domNode.mapNode = this;
- var coord = [x,y];
+# -*- coding: utf-8 -*-
+import sys, math, pygame;
+from enviro import *;
+pygame.init();
+
+def Window(width, height):
+ return pygame.display.set_mode((width, height));
+
+class World:
+ map = [];
  
- this.xy = function(x,y){ //Even row system
-  if(typeof x == "undefined" && typeof y == "undefined"){
-   return [coord[0], coord[1]];
-  }else{
-   World.map[coord[0], coord[1]] = undefined;
-   if(!World.map[x]){World.map[x]=[];}
-   World.map[x,y] = this;
+ def xy(x,y):
+  try:
+   return World.map[x][y];
+  except IndexError:
+   return False;
+ 
+ def qr(q,r):
+  try:
+   return World.map[r][q+math.ceil(r/2)];
+  except IndexError:
+   return False;
+
+class Cell:
+ def __init__(self,x,y):
+  self.coord = [x,y];
+  self.domNode = Element("cell");
+ 
+ def xy(self,x,y): #Even row system
+  try:
+   x,y;
+   try:
+    del World.map[coord[0]][coord[1]];
+   except IndexError:
+    pass;
    coord = [x,y];
-  }
- }
+   World.map[x][y] = self;
+  except NameError:
+   return [coord[0], coord[1]];
  
- this.qr = function(q,r){ //Axial system
-  if(typeof q == "undefined" && typeof r == "undefined"){
-   return [coord[1] - Math.floor(coord[0]/2), coord[0]];
-  }else{
-   var y = q+Math.ceil(r/2);
-   World.map[coord[0], coord[1]] = undefined;
-   if(!World.map[r]){World.map[r]=[];}
-   World.map[r,y] = this;
-   coord = [r,y];
-  }
- }
- 
- this.on = this.domNode.addEventListener;
- this.xy(x,y);
-}
