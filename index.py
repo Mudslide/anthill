682fa22b041717fead0f219d33c9cef119e6ae50
m93a <m93a.cz@gmail.com>
2013-11-11 12:15:45 +0100

    js -> py

--- index.py
+++ index.py
@@ -1,12 +1,7 @@
-<?xml version="1.0" encoding="utf-8" ?>
-<html:html xmlns:html="http://www.w3.org/1999/xhtml">
- <html:head>
-  <html:title>Hexagon</html:title>
-  <html:link rel="stylesheet" href="./style.css" />
-  <html:script src="./enviro.js" />
-  <html:script src="./engine.js" />
-  <html:script data-width="5" data-height="3" src="./modules/maps/empty.js" />
- </html:head>
- <html:body>
- </html:body>
-</html:html>
+# -*- coding: utf-8 -*-
+from engine import *;
+Window(800,600);
+
+while 1:
+ pygame.time.wait(25);
+ Cell(0,0);
--- modules/maps/empty.py
+++ modules/maps/empty.py
@@ -1,20 +1,2 @@
-(function(){
+# -*- coding: utf-8 -*-
 
-
-var width  = parseInt(doc.currentScript.getAttr("data-width" ));
-var height = parseInt(doc.currentScript.getAttr("data-height"));
-
-    win.on("load",function(){
-     var i = -1, j = -1, row;
-     while(i++<height){
-      row = doc.body.mkChild("","row");
-      j = 0;
-      while(j++<width){
-       row.addChild(
-        (new Node(j,i)).domNode
-       );
-      }
-     }
-    });
-
-})();
